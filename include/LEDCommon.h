675425ac695d10b07138116dda0072a14a6f067a
ll2lab <service.lab.git@eyles.me.uk>
2024-11-17 16:04:06 +0000

    Minor edit

--- include/LEDCommon.h
+++ include/LEDCommon.h
@@ -107,16 +107,16 @@ struct PacketHeader{                     // ESP_Now packet header
 
 // SHARED FRAME TYPES, CONSTANTS AND STRUCTURES
 
-#define FRAME_TYPE_COUNT         9UL     // Frame type count
-#define FRAME_TYPE_OTAINIT       0UL     // Initialise OTA mode
-#define FRAME_TYPE_ANALYSERDATA  1UL     // Analyser data frame 
-#define FRAME_TYPE_PANELCONTROL  2UL     // Panel control frame
-#define FRAME_TYPE_SETCONFIG     3UL     // Set config frame
-#define FRAME_TYPE_PANELCONFIG   4UL     // Panel config frame
-#define FRAME_TYPE_BOARDCONFIG   5UL     // Board config frame
-#define FRAME_TYPE_SCENECONFIG   6UL     // Scene config frame
-#define FRAME_TYPE_VISUALCONFIG  7UL     // Visual config frame
-#define FRAME_TYPE_PULSECONFIG   8UL     // Pulse config frame 
+#define FRAME_TYPE_COUNT         19UL     // Frame type count
+#define FRAME_TYPE_OTAINIT       10UL     // Initialise OTA mode
+#define FRAME_TYPE_ANALYSERDATA  11UL     // Analyser data frame 
+#define FRAME_TYPE_PANELCONTROL  12UL     // Panel control frame
+#define FRAME_TYPE_SETCONFIG     13UL     // Set config frame
+#define FRAME_TYPE_PANELCONFIG   14UL     // Panel config frame
+#define FRAME_TYPE_BOARDCONFIG   15UL     // Board config frame
+#define FRAME_TYPE_SCENECONFIG   16UL     // Scene config frame
+#define FRAME_TYPE_VISUALCONFIG  17UL     // Visual config frame
+#define FRAME_TYPE_PULSECONFIG   18UL     // Pulse config frame 
 
 #define LED_MAP_SET              0UL     // Map for set (not yet used)
 #define LED_MAP_PANEL            1UL     // Map for panel
